40a11e7512f67220d9555c24808a408d0e442bd4
dchu07 <37084052+dchu07@users.noreply.github.com>
2019-09-18 18:52:47 -0400

    session04 exercise 1 update

--- Session04/calc.py
+++ Session04/calc.py
@@ -1,17 +1,14 @@
 #Session04 Exercise01
 print('Session04 Exercise01:')
 
-from math import sqrt
+import math
 
-a = float(input("a= "))
-b = float(input("b= "))
-c = float(input("c= "))
 def quadratic(a,b,c):
-    formula = b**2 -(4*a*c)
-    if formula >= 0:
-        return ("x= ",(-b + sqrt(formula))/(2*a), "x= ",(-b - sqrt(formula))/(2*a))
-    if formula < 0:
-        return ("x= ",-b/(2*a),"+",sqrt(formula*(-1))/(2*a),"i" 
-                "x= ",-b/(2*a),"-",sqrt(formula*(-1))/(2*a),"i")
+    try:
+        x = (-b+(b**2-4*a*c)**(1/2))/(2*a)
+        x2 = (-b+(b**2-4*a*c)**(1/2))/(2*a)
+        print(f"The value of x are: {x:.2f} and {x2:.2f}")
+    except TypeError:
+        print("Oops something is wrong")
 
-print(quadratic(a,b,c))
+quadratic(1,-5,"b")
