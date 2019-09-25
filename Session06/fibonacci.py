85bdff492eb3bbf9948fceceb8812d9ac3c99e2c
dchu07 <37084052+dchu07@users.noreply.github.com>
2019-09-25 19:18:05 -0400

    session06 exercise02

--- Session06/fibonacci.py
+++ Session06/fibonacci.py
@@ -1,9 +1,7 @@
 #Question 2
 #Write a program, fibonacci.py to compute the Fibonacci number of an integer , n.
 def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1 or n == 2:
+    if n == 1 or n == 2:
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
@@ -11,5 +9,7 @@ def fibonacci(n):
 n = int(input("Please enter a number to find its fibonacci: "))
 if n < 0:
     print("Fibonacci does not exist for negative numbers.")
+elif n == 0:
+    print("The fibonacci of 0 is: 1")
 else:
-    print("The fibonacci of", n, "is: ", fibonacci(n))
+    print("The fibonacci of", n, "is:", fibonacci(n))
