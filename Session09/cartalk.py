56e7ef7c7dcd4d9e106d78c67b7b3605bd47b220
dchu07 <37084052+dchu07@users.noreply.github.com>
2019-10-04 19:46:09 -0400

    cartalk.py

---
+++ Session09/cartalk.py
@@ -0,0 +1,20 @@
+def three_consecutive_double_words(word):
+    index = 0
+    while index < len(word)-5:
+        if word[index] == word[index+1] and word[index+2] == word[index+3] and word[index+4] == word[index+5]:
+            return True
+        index += 1
+    return False
+
+def find_three_consecutive_double_words():
+    f = open("Session09/words.txt")
+    count = 0
+    for line in f:
+        word = line.strip()
+        if three_consecutive_double_words(word):
+            print(word)
+            count += 1
+    print(f"There are {count} words with three consecutive double words")
+
+print(find_three_consecutive_double_words())
+
